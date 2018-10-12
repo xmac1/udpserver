53b1b522f7e9c0159a185bcd5850a5a46e15f390
char <char@aladinfun.com>
2018-10-12 22:15:26 +0800

    make file

---
+++ Makefile
@@ -0,0 +1,2 @@
+udpserver:
+	g++ -o build/udpserver src/main.cpp -L./libs/unp -I./inlude
--- src/main.cpp
+++ src/main.cpp
@@ -7,4 +7,35 @@ int main(int argc, char** argv) {
 	struct sockaddr_in sockaddr;
 
 	if (argc != 2) {
+		err_quit("usage: a.out <IPaddress>");
+	}
+
+	if (sockfd = socket(AF_INET, SOCK_STREAM, 0) < 0) {
+		err_sys("socket error");
+	}
+
+	bzero(&sockaddr, sizeof(sockaddr));
+
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(13);
+	if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) < 0) {
+		err_quit("inet_pton error for %s", argv[1]);
+	}
+
+	if (connect(sockfd, (SA*) &servaddr, sizeof(servaddr)) < 0) {
+		err_sys("connect error");
+	}
+
+	while((n = read(sockfd, recvline, MAXLINE)) > 0) {
+		recvline[n] = 0;
+		if (fputs(recvline, stdout) == EOF) {
+			err_sys("fputs error");
+		}
+	}
+
+	if (n < 0) {
+		err_sys("read error");
+	}
+
+	return 0;
 }
