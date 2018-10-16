515ee59f47c1ef71426378f4c0984989357da3f3
xmac-ubuntu <1743164928@qq.com>
2018-10-16 08:39:54 +0800

    cmake file

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char** argv) {
 	bzero(&servaddr, sizeof(servaddr));
 
 	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(13);
+	servaddr.sin_port = htons(8888);
 	if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) < 0) {
 		err_quit("inet_pton error for %s", argv[1]);
 	}
